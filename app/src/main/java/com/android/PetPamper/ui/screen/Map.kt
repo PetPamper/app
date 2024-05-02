67e01c39c75c47a37919e3c30df8503b552f6a59
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2024-05-02 17:30:07 +0200

    Run KtFmt format Gradle Task
    Format the code using the automatic formatting task

--- app/src/main/java/com/android/PetPamper/ui/screen/Map.kt
+++ app/src/main/java/com/android/PetPamper/ui/screen/Map.kt
@@ -11,25 +11,22 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.android.PetPamper.model.Groomer
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.maps.android.compose.*
 import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
 import com.android.PetPamper.database.FirebaseConnection
 import com.android.PetPamper.model.Address
+import com.android.PetPamper.model.Groomer
 import com.android.PetPamper.model.LocationMap
 import com.android.PetPamper.model.UserViewModel
 import com.android.PetPamper.resources.distance
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-
+import com.google.maps.android.compose.*
 
 @Composable
 fun MapView(email: String) {
@@ -62,85 +59,73 @@ fun MapView(email: String) {
     position = CameraPosition.fromLatLngZoom(LatLng(46.516, 6.63282), 10f)
   }
 
-  GoogleMap(
-    modifier = Modifier.fillMaxSize(),
-    cameraPositionState = cameraPositionState
-  ) {
+  GoogleMap(modifier = Modifier.fillMaxSize(), cameraPositionState = cameraPositionState) {
     groomersNearby.value.forEach { groomer ->
       Marker(
-        state = MarkerState(position = LatLng(groomer.address.location.latitude, groomer.address.location.longitude)),
-        title = groomer.name,
-        snippet = "Click for details",
-        onClick = {
-          selectedGroomer = groomer
-          showDialog = true
-          true // Return true to indicate that we have handled the click
-        }
-      )
+          state =
+              MarkerState(
+                  position =
+                      LatLng(
+                          groomer.address.location.latitude, groomer.address.location.longitude)),
+          title = groomer.name,
+          snippet = "Click for details",
+          onClick = {
+            selectedGroomer = groomer
+            showDialog = true
+            true // Return true to indicate that we have handled the click
+          })
     }
   }
 
   if (showDialog) {
     AlertDialog(
-      onDismissRequest = {
-        showDialog = false
-      },
-      title = {
-        Text(text = "Groomer Details")
-      },
-      text = {
-        selectedGroomer?.let {
-          InfoWindow(it)
-        }
-      },
-      confirmButton = {
-        Button(
-          colors = ButtonColors(Color.LightGray,Color.LightGray,Color.LightGray,Color.LightGray),
-          onClick = {
-            showDialog = false
-          }
-        ) {
-          Text("Close")
-        }
-      }
-    )
+        onDismissRequest = { showDialog = false },
+        title = { Text(text = "Groomer Details") },
+        text = { selectedGroomer?.let { InfoWindow(it) } },
+        confirmButton = {
+          Button(
+              colors =
+                  ButtonColors(Color.LightGray, Color.LightGray, Color.LightGray, Color.LightGray),
+              onClick = { showDialog = false }) {
+                Text("Close")
+              }
+        })
   }
 }
 
 @Composable
 fun InfoWindow(groomer: Groomer) {
-  Column(modifier = Modifier
-    .padding(4.dp)
-    .border(2.dp, Color.White, RoundedCornerShape(40.dp))
-    .padding(8.dp)) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-      Image(
-        painter = rememberImagePainter(
-          request = ImageRequest.Builder(LocalContext.current)
-            .data(groomer.profilePic)
-            .build()
-        ),
-        contentDescription = "Groomer Profile Picture",
-        modifier = Modifier.size(100.dp).padding(4.dp),
-        contentScale = ContentScale.Crop
-      )
-      Column(modifier = Modifier.padding(4.dp)) {
-        Text(
-          text = groomer.name,
-          color = Color(0xFF000080),  // Navy blue color
-          fontSize = 26.sp,
-          fontWeight = FontWeight.Bold
-        )
-        Text(
-          text = "Distance: ${distance(groomer.address.location.latitude, groomer.address.location.longitude,
-            groomer.address.location.latitude, groomer.address.location.longitude )}", //change parameters
-          fontWeight = FontWeight.Bold
-        )
-        Text(
-          text = "Groomable Pets: ${groomer.petTypes.joinToString(", ")}",
-          fontWeight = FontWeight.Bold
-        )
+  Column(
+      modifier =
+          Modifier.padding(4.dp)
+              .border(2.dp, Color.White, RoundedCornerShape(40.dp))
+              .padding(8.dp)) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+          Image(
+              painter =
+                  rememberImagePainter(
+                      request =
+                          ImageRequest.Builder(LocalContext.current)
+                              .data(groomer.profilePic)
+                              .build()),
+              contentDescription = "Groomer Profile Picture",
+              modifier = Modifier.size(100.dp).padding(4.dp),
+              contentScale = ContentScale.Crop)
+          Column(modifier = Modifier.padding(4.dp)) {
+            Text(
+                text = groomer.name,
+                color = Color(0xFF000080), // Navy blue color
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold)
+            Text(
+                text =
+                    "Distance: ${distance(groomer.address.location.latitude, groomer.address.location.longitude,
+            groomer.address.location.latitude, groomer.address.location.longitude )}", // change parameters
+                fontWeight = FontWeight.Bold)
+            Text(
+                text = "Groomable Pets: ${groomer.petTypes.joinToString(", ")}",
+                fontWeight = FontWeight.Bold)
+          }
+        }
       }
-    }
-  }
 }
