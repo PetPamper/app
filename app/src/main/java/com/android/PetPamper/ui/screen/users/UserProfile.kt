2bdaaf0ec80cac0226ef270e0adc3009048dc3a0
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2024-05-02 22:25:26 +0200

    Merge branch 'main' into merging-grommerSign

--- app/src/main/java/com/android/PetPamper/ui/screen/users/UserProfile.kt
+++ app/src/main/java/com/android/PetPamper/ui/screen/users/UserProfile.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -57,11 +58,59 @@ fun UserProfileScreen(userProfile: UserProfile) {
               horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.Start),
               verticalAlignment = Alignment.Top,
           ) {
-            Image(
-                painter = painterResource(id = R.drawable.user_image),
-                contentDescription = "image description",
-                modifier = Modifier.size(width = 174.dp, height = 167.dp),
-                contentScale = ContentScale.FillBounds)
+            Column() {
+              Image(
+                  painter = painterResource(id = R.drawable.user_image),
+                  contentDescription = "User profile image",
+                  modifier = Modifier.size(width = 160.dp, height = 150.dp),
+                  contentScale = ContentScale.FillBounds)
+              Button(
+                  onClick = { /* Handle your click here */},
+                  modifier =
+                      Modifier.width(90.dp) // Adjusted for visual balance
+                          .height(40.dp)
+                          .padding(start = 75.dp),
+                  // Adjusted for visual balance
+                  colors = ButtonDefaults.buttonColors(Color.Transparent),
+                  contentPadding =
+                      PaddingValues(0.dp) // Ensure there's no padding inside the button
+                  ) {
+                    Box(
+                        contentAlignment =
+                            Alignment.Center, // This centers the content inside the Box
+                        modifier =
+                            Modifier.fillMaxSize()
+                                .padding(0.dp) // This makes the Box fill the Button
+                        ) {
+                          Row(
+                              modifier = Modifier.fillMaxSize(), // Ensure the Row fills the Box
+                              verticalAlignment = Alignment.CenterVertically,
+                              horizontalArrangement = Arrangement.Start) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.editphoto),
+                                    contentDescription = "Edit Photo",
+                                    modifier = Modifier.size(15.dp),
+                                    alignment = Alignment.CenterStart,
+                                    contentScale =
+                                        ContentScale
+                                            .Crop // This ensures the image covers the button area
+                                                  // without distortion
+                                    )
+                                Text(
+                                    text = "Edit photo",
+                                    style =
+                                        TextStyle(
+                                            fontSize = 8.sp, // Adjusted for readability
+                                            fontWeight = FontWeight.Bold, // Makes text bold
+                                            color =
+                                                Color
+                                                    .Black // Changed for better visibility against
+                                                           // likely dark images
+                                            ))
+                              }
+                        }
+                  }
+            }
             Column(
                 modifier = Modifier.padding(12.dp), // Adds padding around the entire column content
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -101,70 +150,79 @@ fun UserProfileScreen(userProfile: UserProfile) {
         verticalAlignment = Alignment.Top,
     ) {
       Box(
-          Modifier.shadow(elevation = 4.dp, shape = RoundedCornerShape(32.dp))
-              .width(216.dp)
-              .height(100.dp)
-              .background(color = Color(0xFFF4F3F3), shape = RoundedCornerShape(size = 8.dp))
-              .padding(start = 12.dp, top = 12.dp, end = 12.dp, bottom = 12.dp)) {
-            Text(
-                text = "My PawPoints",
-                modifier = Modifier.width(91.dp).height(24.dp),
-                style =
-                    TextStyle(
-                        fontSize = 13.sp,
-                        lineHeight = 16.sp,
-                        // fontFamily = FontFamily(Font(R.font.roboto slab)),
-                        fontWeight = FontWeight(700),
-                        color = Color(0xFF11347A),
-                    ))
-            Spacer(modifier = Modifier.height(16.dp).padding(12.dp))
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.Start),
-                verticalAlignment = Alignment.Top,
-            ) {
-              Image(
-                  painter = painterResource(id = R.drawable.ellipse1),
-                  contentDescription = "image description",
-                  modifier = Modifier.padding(1.dp).width(58.dp).height(58.dp),
-                  contentScale = ContentScale.None)
-              Column(
-                  modifier =
-                      Modifier.padding(12.dp), // Adds padding around the entire column content
-                  horizontalAlignment = Alignment.CenterHorizontally,
-                  verticalArrangement =
-                      Arrangement.spacedBy(12.dp) // Adds vertical space between the children
-                  ) {
-                    // First Surface block
-                    Surface(
-                        modifier =
-                            Modifier.width(93.dp)
-                                .height(18.dp)
-                                .background(
-                                    color = Color(0x7A2490DF),
-                                    shape = RoundedCornerShape(size = 8.dp))
-                                .padding(start = 12.dp, top = 12.dp, end = 12.dp, bottom = 12.dp),
-                        shape = RoundedCornerShape(8.dp)) {
-                          // Content inside the first Surface
-                          // Example: Padding applied within the Surface content area
+          modifier =
+              Modifier.shadow(elevation = 4.dp, shape = RoundedCornerShape(32.dp))
+                  .width(216.dp)
+                  .height(100.dp)
+                  .background(color = Color(0xFFF4F3F3), shape = RoundedCornerShape(size = 8.dp))
+                  .padding(12.dp) // Simplified padding application
+          ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(12.dp) // Space between Text and Row
+                ) {
+                  Text(
+                      text = "My PawPoints",
+                      style =
+                          TextStyle(
+                              fontSize = 13.sp,
+                              lineHeight = 16.sp,
+                              fontWeight = FontWeight.Bold,
+                              color = Color(0xFF11347A)))
 
-                        }
+                  Row(
+                      horizontalArrangement =
+                          Arrangement.spacedBy(12.dp), // Space between image and column
+                      verticalAlignment = Alignment.CenterVertically) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ellipse1),
+                            contentDescription = "image description",
+                            modifier =
+                                Modifier.size(58.dp).padding(1.dp) // Padding around the image
+                            )
 
-                    // Second Surface block
-                    Surface(
-                        modifier =
-                            Modifier.width(93.dp)
-                                .height(18.dp)
-                                .background(
-                                    color = Color(0x7A2490DF),
-                                    shape = RoundedCornerShape(size = 8.dp))
-                                .padding(start = 12.dp, top = 12.dp, end = 12.dp, bottom = 12.dp),
-                        shape = RoundedCornerShape(8.dp)) {
-                          // Content inside the second Surface
-                          // Example: Padding applied within the Surface content area
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement =
+                                Arrangement.spacedBy(8.dp) // Space between two surface blocks
+                            ) {
+                              Surface(
+                                  modifier = Modifier.width(93.dp).height(18.dp),
+                                  color = Color(0x7A2490DF),
+                                  shape = RoundedCornerShape(8.dp),
+                              ) {
+                                Text(
+                                    modifier = Modifier.width(50.dp).height(11.dp),
+                                    text = "My rewards",
+                                    style =
+                                        TextStyle(
+                                            fontSize = 9.sp,
+                                            lineHeight = 10.8.sp,
+                                            // fontFamily = FontFamily(Font(R.font.raleway)),
+                                            fontWeight = FontWeight(700),
+                                            color = Color(0xFF11347A),
+                                            textAlign = TextAlign.Center))
+                              }
 
-                        }
-                  }
-            }
+                              Surface(
+                                  modifier = Modifier.width(93.dp).height(18.dp),
+                                  shape = RoundedCornerShape(8.dp),
+                                  color = Color(0x7A2490DF)) {
+                                    Text(
+                                        modifier = Modifier.width(61.dp).height(7.dp),
+                                        text = "My reviews",
+                                        style =
+                                            TextStyle(
+                                                fontSize = 9.sp,
+                                                lineHeight = 10.8.sp,
+                                                // fontFamily = FontFamily(Font(R.font.raleway)),
+                                                fontWeight = FontWeight(700),
+                                                color = Color(0xFF11347A),
+                                                textAlign = TextAlign.Center,
+                                            ))
+                                  }
+                            }
+                      }
+                }
           }
       Column(
           modifier = Modifier.padding(12.dp), // Adds padding around the entire column content
@@ -199,7 +257,8 @@ fun UserProfileScreen(userProfile: UserProfile) {
                             // fontFamily = FontFamily(Font(R.font.raleway)),
                             fontWeight = FontWeight(400),
                             color = Color(0xFF11347A),
-                        ))
+                            textAlign = TextAlign.Center))
+
                 Image(
                     painter = painterResource(id = R.drawable.paw),
                     contentDescription = "image description",
@@ -234,7 +293,8 @@ fun UserProfileScreen(userProfile: UserProfile) {
                             // fontFamily = FontFamily(Font(R.font.raleway)),
                             fontWeight = FontWeight(400),
                             color = Color(0xFF11347A),
-                        ))
+                            textAlign = TextAlign.Center))
+
                 Image(
                     painter = painterResource(id = R.drawable.history),
                     contentDescription = "image description",
@@ -245,25 +305,40 @@ fun UserProfileScreen(userProfile: UserProfile) {
     }
     Spacer(modifier = Modifier.height(4.dp))
     Box(
-        Modifier.shadow(elevation = 4.dp, shape = RoundedCornerShape(32.dp))
-            .width(395.dp)
-            .height(43.dp)
-            .background(color = Color(0xFFF4F3F3), shape = RoundedCornerShape(size = 8.dp))
-            .padding(12.dp),
-    ) {
-      Text(
-          text = "Edit profile",
-          modifier = Modifier.width(367.dp).height(22.dp),
-          style =
-              TextStyle(
-                  fontSize = 15.sp,
-                  lineHeight = 18.sp,
-                  // fontFamily = FontFamily(Font(R.font.raleway)),
-                  fontWeight = FontWeight(700),
-                  color = Color(0xFF11347A),
-                  textAlign = TextAlign.Center,
-              ))
-    }
+        contentAlignment = Alignment.Center, // Centers the Text over the Button
+        modifier =
+            Modifier.width(395.dp)
+                .height(43.dp)
+                .background(color = Color(0xFFF4F3F3), shape = RoundedCornerShape(size = 8.dp))) {
+          Button(
+              onClick = { /* Handle your click here */},
+              modifier =
+                  Modifier.matchParentSize(), // Ensures the Button matches the size of the Box
+              colors =
+                  ButtonDefaults.buttonColors(Color.Transparent), // Makes the Button transparent
+              elevation =
+                  ButtonDefaults.elevation(
+                      defaultElevation = 0.dp,
+                      pressedElevation = 0.dp,
+                      hoveredElevation = 0.dp,
+                      focusedElevation = 0.dp) // Removes elevation to avoid shadow
+              ) {
+                // Empty content here, as we're overlaying the Text in the Box scope
+              }
+          Text(
+              text = "Edit profile",
+              style =
+                  TextStyle(
+                      fontSize = 15.sp,
+                      lineHeight = 18.sp,
+                      fontWeight = FontWeight(700),
+                      color = Color(0xFF11347A),
+                      textAlign = TextAlign.Center),
+              modifier =
+                  Modifier.align(
+                      Alignment.Center) // Ensures the Text is centered in the Box, over the Button
+              )
+        }
     Spacer(modifier = Modifier.height(4.dp))
     // Two smaller squares underneath
     Row(
