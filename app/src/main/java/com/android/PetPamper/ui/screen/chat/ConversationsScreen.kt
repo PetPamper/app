53c64cd4ce215b1aba73e34c873bf93a0a3f3c1f
Ali Bentaleb <ali.bentaleb@epfl.ch>
2024-05-03 22:51:43 +0200

    Add GitHub Actions workflow to build APK

--- app/src/main/java/com/android/PetPamper/ui/screen/chat/ConversationsScreen.kt
+++ app/src/main/java/com/android/PetPamper/ui/screen/chat/ConversationsScreen.kt
@@ -1,6 +1,5 @@
 package com.android.PetPamper.ui.screen.chat
 
-
 import android.annotation.SuppressLint
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
@@ -39,64 +38,64 @@ import com.android.PetPamper.R
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun ConversationsScreen(onBackPressed: () -> Unit, navController: NavController) {
-    val pets =
-        listOf(
-            Pet(
-                name = "Simo",
-                description = "Ba hwa hassan 2",
-                dateOfBirth = "2018-05-12",
-                pictureRes = R.drawable.placeholder),
-            Pet(
-                name = "Hitler",
-                description = "Knt rssam",
-                dateOfBirth = "2020-03-01",
-                pictureRes = R.drawable.placeholder),
-            // Add more pets as needed
-        )
+  val pets =
+      listOf(
+          Pet(
+              name = "Simo",
+              description = "Ba hwa hassan 2",
+              dateOfBirth = "2018-05-12",
+              pictureRes = R.drawable.placeholder),
+          Pet(
+              name = "Hitler",
+              description = "Knt rssam",
+              dateOfBirth = "2020-03-01",
+              pictureRes = R.drawable.placeholder),
+          // Add more pets as needed
+      )
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Messages") },
-                navigationIcon = {
-                    IconButton(onClick = onBackPressed) {
-                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back")
-                    }
-                })
-        },
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = { navController.navigate("UsersScreen") },
-                backgroundColor = Color.Blue,
-                contentColor = Color.White,
-                content = {
-                    Icon(Icons.Filled.Add, "")
-                }
-            )
-        },) {
-        LazyColumn(
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)) {
-            items(pets) { pet -> PetCard(pet, navController) }
+  Scaffold(
+      topBar = {
+        TopAppBar(
+            title = { Text("Messages") },
+            navigationIcon = {
+              IconButton(onClick = onBackPressed) {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back")
+              }
+            })
+      },
+      floatingActionButton = {
+        FloatingActionButton(
+            onClick = { navController.navigate("UsersScreen") },
+            backgroundColor = Color.Blue,
+            contentColor = Color.White,
+            content = { Icon(Icons.Filled.Add, "") })
+      },
+  ) {
+    LazyColumn(
+        contentPadding = PaddingValues(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp)) {
+          items(pets) { pet -> PetCard(pet, navController) }
         }
-    }
+  }
 }
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PetCard(pet: Pet, navController: NavController) {
-    Card(modifier = Modifier.fillMaxWidth().padding(1.dp), elevation = 4.dp, onClick = {navController.navigate("ChatScreen")}) {
+  Card(
+      modifier = Modifier.fillMaxWidth().padding(1.dp),
+      elevation = 4.dp,
+      onClick = { navController.navigate("ChatScreen") }) {
         Row(modifier = Modifier.padding(1.dp), verticalAlignment = Alignment.CenterVertically) {
-            Image(
-                painter = painterResource(id = pet.pictureRes),
-                contentDescription = null,
-                modifier = Modifier.size(64.dp).clip(CircleShape))
-            Column(modifier = Modifier.weight(1f).padding(start = 16.dp)) {
-                Text(text = pet.name, style = MaterialTheme.typography.subtitle1)
-                Text(text = pet.description, style = MaterialTheme.typography.body2)
-            }
+          Image(
+              painter = painterResource(id = pet.pictureRes),
+              contentDescription = null,
+              modifier = Modifier.size(64.dp).clip(CircleShape))
+          Column(modifier = Modifier.weight(1f).padding(start = 16.dp)) {
+            Text(text = pet.name, style = MaterialTheme.typography.subtitle1)
+            Text(text = pet.description, style = MaterialTheme.typography.body2)
+          }
         }
-    }
+      }
 }
 
 data class Pet(
